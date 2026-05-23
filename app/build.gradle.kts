527eefe0b4b00466f135fcec36ad7e904b421e97
Bouric0076 <bouricenos0@gmail.com>
2026-05-23 14:45:48 +0300

    Bump app version to 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.ics2300.pocketbudget"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
